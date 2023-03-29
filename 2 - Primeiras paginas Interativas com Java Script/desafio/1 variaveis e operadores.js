934800f7d02023f65c700016c89ae45be528ad20
AlexSan1910 <alex.gsan1@gmail.com>
2023-03-29 16:56:09 -0300

    Segundo Etapa do bootcammp

---
+++ 2 - Primeiras paginas Interativas com Java Script/desafio/1 variaveis e operadores.js
@@ -0,0 +1,10 @@
+
+let precoCombustivel = 5.39;
+let mediaGasto = 12;
+let kmViagem = 100;
+
+let valorGastoPorKm = precoCombustivel / mediaGasto;
+
+let total = valorGastoPorKm * kmViagem;
+
+console.log(total);
