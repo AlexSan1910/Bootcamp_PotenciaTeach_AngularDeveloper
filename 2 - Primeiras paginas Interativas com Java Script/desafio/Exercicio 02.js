f780dd03aea15389ad5871d23867dff8ccddb689
AlexSan1910 <alex.gsan1@gmail.com>
2023-03-30 09:02:49 -0300

    Alterações exercicios

--- 2 - Primeiras paginas Interativas com Java Script/desafio/Exercicio 02.js
+++ 2 - Primeiras paginas Interativas com Java Script/desafio/Exercicio 02.js
@@ -21,7 +21,7 @@ let imc = peso / (altura * altura);
 
 if(imc < 18.5){
     console.log("Abaixo do peso");
-}else if(imc > 18.5 && imc < 25){
+}else if(imc >= 18.5 && imc <= 25){
     console.log(" Peso normal");
 }else if(imc > 25 && imc < 30){
     console.log("Acima do Peso");
